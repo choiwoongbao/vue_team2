e1f8814d6bf2f85711f308102b6a077cc6b0641e
uixhyeon <uixhyeon@gmail.com>
2025-10-16 15:55:11 +0900

    scss 설정

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,21 @@
 <script setup>
-import Banner from './mains/Banner.vue';
-import Faq from './mains/Faq.vue';
-import Info from './mains/Info.vue';
-import QuickR from './mains/QuickR.vue';
-import Serch from './mains/Serch.vue';
-
-
+import Banner from "./mains/Banner.vue";
+import Faq from "./mains/Faq.vue";
+import About from "./mains/About.vue";
+import Info from "./mains/Info.vue";
+import QuickR from "./mains/QuickR.vue";
+import Serch from "./mains/Serch.vue";
 </script>
 
 <template>
-  <div >
-home
-  </div>
+  <div>home</div>
 
-<Banner/>
-<QuickR/>
-<Serch/>
-<Info/>
-<Faq/>
-
-
+  <Banner />
+  <QuickR />
+  <About />
+  <Serch />
+  <Info />
+  <Faq />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
